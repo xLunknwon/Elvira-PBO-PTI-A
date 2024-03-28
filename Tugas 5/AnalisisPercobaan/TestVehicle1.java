b935e0682dd3410d33735585dee736cd78c59292
Elvira <elvirarosa304@gmail.com>
2024-03-28 23:29:07 +0700

    kakk udah kakk[CRY]

--- Tugas 5/AnalisisPercobaan/TestVehicle1.java
+++ Tugas 5/AnalisisPercobaan/TestVehicle1.java
@@ -3,7 +3,6 @@ public class TestVehicle1 {
         System.out.println("Creating a vehicle with a 10,000 kg maximumload.");
         Vehicle1 vehicle = new Vehicle1(10000);
         System.out.println("Add box #1 (500kg) : " + vehicle.addBox(500));
-        System.out.println("Add load(100kg) : " + (vehicle.load=500));
         System.out.println("Add box #2 (250kg) : " + vehicle.addBox(250));
         System.out.println("Add box #3 (5000kg) : " + vehicle.addBox(5000));
         System.out.println("Add box #4 (4000kg) : " + vehicle.addBox(4000));
