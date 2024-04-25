519a754719a88daa9e1ba8329bf49113845b5fcc
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2024-04-25 09:26:52 +0700

    add: ppt lama

--- source/pertemuan7/Pengajar.java
+++
@@ -1,7 +0,0 @@
-package source.pertemuan7.studikasus1;
-
-public class Pengajar extends Civitas {
-    void menilai() { }
-    void mengajar() { }
-    void ngasihTugas() { }
-}
--- source/pertemuan7/studikasus1/Main.java
+++ source/pertemuan7/studikasus1/Main.java
@@ -16,7 +16,5 @@ public class Main {
         Asprak adit = new Asprak();
 
         adit.presentasi();
-
-        
     }
 }
